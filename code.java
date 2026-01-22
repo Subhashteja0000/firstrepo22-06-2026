c2234077a17fc7f90355fbcd290f2e88f39472ca
@subhashtejamurarisetty <65499581+Subhashteja0000@users.noreply.github.com>
2026-01-22 21:21:15 +0530

    Add welcome message to main method

--- code.java
+++ code.java
@@ -1,5 +1,6 @@
 public class code {
     public static void main(String[] args) {
         System.out.println("Hello World, welcome tothe DevOps world!");
+        System.out.println("Hello World, welcome to my world");
     }
 }
